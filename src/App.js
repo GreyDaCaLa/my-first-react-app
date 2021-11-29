2035f17d5bfd5966c96f129462da21e6ce37b772
GreyDaCaLa <gdele9164@gmail.com>
2021-11-29 13:40:09 -0500

    Turning in

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>My First React App</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ya Boii Just Took His First Steps Into React
         </p>
+        <p>And Another One</p>
         <a
           className="App-link"
           href="https://reactjs.org"
